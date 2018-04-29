850140206a0fd44f4658936ec50d07983a85d681
tux3 <barrdetwix@gmail.com>
2018-04-29 04:15:46 +0200

    Cache the global object used for each module instantiation

--- global.cpp
+++ global.cpp
@@ -13,33 +13,39 @@ Local<Context> prepareGlobalContext(IsolateWrapper& isolateWrapper)
     Isolate::Scope isolateScope(isolate);
     EscapableHandleScope handleScope(isolate);
 
-    Local<ObjectTemplate> globalTemplate = ObjectTemplate::New(isolate);
-    Local<Context> context = Context::New(isolate, {}, globalTemplate);
-    Context::Scope contextScope(context);
-    Local<Object> global = context->Global();
+    static v8::Persistent<v8::Context> persistentContext;
 
-    global->Set(String::NewFromUtf8(isolate, "global"), global);
+    if (persistentContext.IsEmpty()) {
+        Local<ObjectTemplate> globalTemplate = ObjectTemplate::New(isolate);
+        Local<Context> context = Context::New(isolate, {}, globalTemplate);
+        Context::Scope contextScope(context);
+        Local<Object> global = context->Global();
 
-    // Set up exports and module.exports
-    Local<Object> exportsObj = Object::New(isolate);
-    Local<Object> moduleObj = Object::New(isolate);
-    moduleObj->Set(String::NewFromUtf8(isolate, "exports"), exportsObj);
-    global->Set(String::NewFromUtf8(isolate, "exports"), exportsObj);
-    global->Set(String::NewFromUtf8(isolate, "module"), moduleObj);
+        global->Set(String::NewFromUtf8(isolate, "global"), global);
 
-    // Load all native modules into global
-    vector nativeModules = NativeModule::getNativeModuleNames();
-    for (string name : nativeModules) {
-        NativeModule mod(isolateWrapper, name);
-        Local<Object> exports = mod.getExports();
-        global->Set(String::NewFromUtf8(isolate, name.c_str()), exports);
+        // Set up exports and module.exports
+        Local<Object> exportsObj = Object::New(isolate);
+        Local<Object> moduleObj = Object::New(isolate);
+        moduleObj->Set(String::NewFromUtf8(isolate, "exports"), exportsObj);
+        global->Set(String::NewFromUtf8(isolate, "exports"), exportsObj);
+        global->Set(String::NewFromUtf8(isolate, "module"), moduleObj);
+
+        // Load all native modules into global
+        vector nativeModules = NativeModule::getNativeModuleNames();
+        for (string name : nativeModules) {
+            NativeModule mod(isolateWrapper, name);
+            Local<Object> exports = mod.getExports();
+            global->Set(String::NewFromUtf8(isolate, name.c_str()), exports);
+        }
+
+        // Load Buffer, the only global class that Node injects...
+        // (yes, even though it's also available through the global buffer.Buffer)
+        NativeModule bufferMod(isolateWrapper, "buffer");
+        auto bufferClassStr = String::NewFromUtf8(isolate, "Buffer");
+        global->Set(bufferClassStr, bufferMod.getExports()->Get(bufferClassStr));
+
+        persistentContext.Reset(isolate, context);
     }
 
-    // Load Buffer, the only global class that Node injects...
-    // (yes, even though it's also available through the global buffer.Buffer)
-    NativeModule bufferMod(isolateWrapper, "buffer");
-    auto bufferClassStr = String::NewFromUtf8(isolate, "Buffer");
-    global->Set(bufferClassStr, bufferMod.getExports()->Get(bufferClassStr));
-
-    return handleScope.Escape(context);
+    return handleScope.Escape(persistentContext.Get(isolate));
 }
